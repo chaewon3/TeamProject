e5df91e55185d09387b3ff7ca121e81ab27589de
HyunggyuPyo <vygudrb@naver.com>
2024-07-25 19:46:06 +0900

    사운드 추가

--- Assets/Scripts/PlayerSound.cs
+++ Assets/Scripts/PlayerSound.cs
@@ -4,43 +4,43 @@ using UnityEngine;
 
 public class PlayerSound : MonoBehaviour
 {
-    AudioSource audio;
+    AudioSource playerAudio;
     Animator ani;
     public AudioClip[] clip;
 
     void Awake()
     {
-        audio = GetComponent<AudioSource>();
+        playerAudio = GetComponent<AudioSource>();
         ani = GetComponent<Animator>();
     }
 
     public void FootRightSound()
     {
-        audio.PlayOneShot(clip[0], 0.2f);
+        playerAudio.PlayOneShot(clip[0], 0.2f);
     }
 
     public void FootLeftSound()
     {
-        audio.PlayOneShot(clip[1], 0.2f);
+        playerAudio.PlayOneShot(clip[1], 0.2f);
     }
 
     public void SwordCombo01Sound()
     {
-        audio.PlayOneShot(clip[2], 0.2f);
+        playerAudio.PlayOneShot(clip[2], 0.2f);
     }
 
     public void SwordCombo02Sound()
     {
-        audio.PlayOneShot(clip[3], 0.2f);
+        playerAudio.PlayOneShot(clip[3], 0.2f);
     }
 
     public void SwordCombo03Sound()
     {
-        audio.PlayOneShot(clip[4], 0.2f);
+        playerAudio.PlayOneShot(clip[4], 0.2f);
     }
 
     public void ArrowSound()
     {
-        audio.PlayOneShot(clip[5], 0.2f);
+        playerAudio.PlayOneShot(clip[5], 0.2f);
     }
 }
